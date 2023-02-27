218c6929e3444232928869fd46e3ed5c051d3d39
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-02-26 23:37:25 -0500

    Remove unnecessary screens and imports

--- src/screens/main/index.ts
+++ src/screens/main/index.ts
@@ -1,5 +1,4 @@
 export { default as EditProfileScreen } from './EditProfileScreen';
-export { default as HistoryScreen } from './HistoryScreen';
 export { default as MainScreen } from './MainScreen';
 export { default as MapScreen } from './MapScreen';
 export { default as RatingScreen } from './RatingScreen';
