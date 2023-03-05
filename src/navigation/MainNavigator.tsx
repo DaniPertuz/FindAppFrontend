643411b9cc1f9ee8649e4d3b4c0bce9c6d1f2dd2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-03-04 21:42:40 -0500

    Remove unused screens and update background color

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import { MainScreen, MapScreen, RatingScreen, HistoryScreen } from '../screens';
+import { MainScreen, MapScreen, RatingScreen } from '../screens';
 
-export type RootStackParams = {
+type RootStackParams = {
     MainScreen: undefined,
     MapScreen: undefined,
-    RatingScreen: undefined,
-    HistoryScreen: undefined,
-    HistoryItemScreen: undefined
+    RatingScreen: undefined
 };
 
 const Stack = createStackNavigator<RootStackParams>();
@@ -22,14 +20,13 @@ export const MainNavigator = () => {
                     shadowColor: 'transparent'
                 },
                 cardStyle: {
-                    backgroundColor: 'white'
+                    backgroundColor: '#FFFFFF'
                 }
             }}
         >
             <Stack.Screen name="MainScreen" options={{ title: '' }} component={MainScreen} />
             <Stack.Screen name="MapScreen" options={{ title: '' }} component={MapScreen} />
             <Stack.Screen name="RatingScreen" options={{ title: '' }} component={RatingScreen} />
-            <Stack.Screen name="HistoryScreen" options={{ title: '' }} component={HistoryScreen} />
         </Stack.Navigator>
     );
 };
