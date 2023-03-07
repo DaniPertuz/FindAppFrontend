d89271c1530d37608afef1d4b313e1100a61dd06
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-03-07 13:48:01 -0500

    Remove export

--- src/navigation/EditProfileNavigator.tsx
+++ src/navigation/EditProfileNavigator.tsx
@@ -7,7 +7,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 import { EditProfileScreen } from '../screens';
 
-export type RootStackParams = {
+type RootStackParams = {
     EditProfileScreen: undefined,
     MapScreen: undefined;
 };
