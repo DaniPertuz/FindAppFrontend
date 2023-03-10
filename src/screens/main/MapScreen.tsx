960748d8aa280211f7f32a52792796732b6187d5
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-03-10 12:37:49 -0500

    Add and configure react-native-maps

--- src/screens/main/MapScreen.tsx
+++ src/screens/main/MapScreen.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const navFocusListener = navigation.addListener('blur', () => {
+            navigation.goBack();
+        });
+    
+        return navFocusListener;
+    }, []);
+
     return (
-        <Text>MapScreen</Text>
+        <MapView
+            style={{
+                height: '100%',
+                width: '100%',
+            }}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+        />
     );
 };
 
