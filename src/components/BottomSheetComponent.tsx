73a1a6b023ee078c4688340f88453bf95bfec211
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-03-17 00:21:55 -0500

    Add maps navigation

--- src/components/BottomSheetComponent.tsx
+++ src/components/BottomSheetComponent.tsx
@@ -7,6 +7,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import moment from 'moment';
 
 import { styles } from '../theme/AppTheme';
+import { useNavigation } from '@react-navigation/native';
 
 interface Props {
     item?: any;
@@ -16,14 +17,16 @@ interface Props {
 
 const BottomSheetComponent = ({ item, bottomSheetRef, snapPoints }: Props) => {
 
+    const navigation = useNavigation();
+
     const copyToClipboard = (text: string) => {
         Clipboard.setString(text);
         bottomSheetRef.current?.close();
     };
 
     const formatDate = (item: any) => {
-        return moment(item.date).format('MMMM DD, YYYY h:mm A').charAt(0).toUpperCase() + moment(item.date).format('MMMM DD, YYYY h:mm A').slice(1)
-    }
+        return moment(item.date).format('MMMM DD, YYYY h:mm A').charAt(0).toUpperCase() + moment(item.date).format('MMMM DD, YYYY h:mm A').slice(1);
+    };
 
     return (
         <BottomSheet
@@ -60,8 +63,26 @@ const BottomSheetComponent = ({ item, bottomSheetRef, snapPoints }: Props) => {
                 <View
                     style={styles.bottomSheetDetailsContainer}
                 >
-                    <Text style={styles.bottomSheetDetailsPrimaryFontStyle}>Lugar</Text>
-                    <Text style={styles.bottomSheetDetailsSecondaryFontStyle}>{item.place.name}</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style={{ width: '90%' }}>
+                            <Text style={styles.bottomSheetDetailsPrimaryFontStyle}>Lugar</Text>
+                            <View style={styles.bottomSheetDetailsSecondaryContainer}>
+                                <Text style={styles.bottomSheetDetailsSecondaryFontStyle}>{item.place.name}</Text>
+                            </View>
+                        </View>
+                        <View style={{ width: '10%' }}>
+                            <TouchableOpacity
+                                onPress={() => {navigation.navigate('MapScreen', { place: item.place.address })}}
+                            >
+                                <Icon
+                                    color='#000000'
+                                    name='arrow-forward-circle-outline'
+                                    size={30}
+                                    style={{ alignSelf: 'center' }}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                     <Text style={styles.bottomSheetDetailsPrimaryFontStyle}>Dirección</Text>
                     <View style={styles.bottomSheetDetailsSecondaryContainer}>
                         <Text style={styles.bottomSheetDetailsSecondaryFontStyle}>{item.place.address}</Text>
