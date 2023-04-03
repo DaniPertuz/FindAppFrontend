8c86c1d29c03b47c45e459c56d6bc2ee81105df9
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-04-03 00:05:34 -0500

    Add navigation to reviews screen and update styles

--- src/components/SavedPlace.tsx
+++ src/components/SavedPlace.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Image, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Image, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Rating } from 'react-native-ratings';
 import { styles } from '../theme/AppTheme';
 
@@ -8,7 +9,10 @@ interface Props {
     onPress: () => void;
 }
 
-const SavedPlace = ({item, onPress}: Props) => {
+const SavedPlace = ({ item, onPress }: Props) => {
+
+    const navigator = useNavigation();
+
     return (
         <TouchableWithoutFeedback
             onPress={onPress}
@@ -18,31 +22,48 @@ const SavedPlace = ({item, onPress}: Props) => {
             >
                 <Image
                     source={{ uri: item.place.photo }}
-                    style={styles.favoriteItemIcon}
+                    style={styles.itemIcon}
                 />
                 <Text style={{
                     ...styles.blackPrimaryFontStyle,
-                    flex: 3,
+                    flex: 4,
                     marginHorizontal: 10,
                     paddingEnd: 20
                 }}>
                     {item.place.name}
                 </Text>
-                <Rating
-                    fractions={2}
-                    imageSize={20}
-                    minValue={1}
-                    ratingBackgroundColor='#FFFFFF'
-                    ratingCount={5}
-                    ratingTextColor='#5856D6'
-                    readonly
-                    showReadOnlyText={false}
-                    showRating
-                    startingValue={item.place.rate}
-                    style={{ flex: 2 }}
-                    tintColor='#EBEBEB'
-                    type='star'
-                />
+                <View
+                    style={{
+                        flex: 2,
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <TouchableOpacity
+                        style={{
+                            alignItems: 'center', flex: 1
+                        }}
+                        activeOpacity={0.9}
+                        onPress={() => navigator.navigate('ReviewsScreen', { place: item.place._id })}
+                    >
+                        <Text style={styles.linkStyle}>
+                            {item.place.rate}
+                        </Text>
+                    <Rating
+                        fractions={2}
+                        imageSize={20}
+                        minValue={1}
+                        ratingBackgroundColor='#FFFFFF'
+                        ratingCount={5}
+                        ratingTextColor='#5856D6'
+                        readonly
+                        showReadOnlyText={false}
+                        startingValue={item.place.rate}
+                        style={{ flex: 2 }}
+                        tintColor='#EBEBEB'
+                        type='star'
+                    />
+                    </TouchableOpacity>
+                </View>
             </View>
         </TouchableWithoutFeedback>
     );
