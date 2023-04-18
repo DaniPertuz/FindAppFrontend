f80450bc0bf1bd4e5f2ab15eb9b4a23324f14d2e
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-04-17 23:29:36 -0500

    Set place data and display string used to search

--- src/screens/main/history/HistoryItem.tsx
+++ src/screens/main/history/HistoryItem.tsx
@@ -4,7 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Rating } from 'react-native-ratings';
 
 import { PlacesContext } from '../../../context/places/PlacesContext';
-import { IService } from '../../../interfaces/app-interfaces';
+import { IPlace, IService } from '../../../interfaces/app-interfaces';
 import { styles } from '../../../theme/AppTheme';
 
 interface Props {
@@ -16,70 +16,80 @@ const HistoryItem = ({ item, onPress }: Props) => {
 
     const navigator = useNavigation();
 
-    const { getPlaceRating } = useContext(PlacesContext);
+    const { getPlaceRating, loadPlaceByID } = useContext(PlacesContext);
 
+    const [place, setPlace] = useState<IPlace>();
     const [placeRating, setPlaceRating] = useState<number>(0);
 
     const setRating = async () => {
         setPlaceRating(await getPlaceRating(item.place._id));
-    }
+    };
+
+    const setPlaceData = async () => {
+        const data = await loadPlaceByID(item.place._id);
+        setPlace(data);
+    };
 
     useEffect(() => {
-      setRating();
+        setPlaceData();
+        setRating();
     }, []);
 
     return (
         <TouchableWithoutFeedback
             onPress={onPress}
         >
-            <View
-                style={styles.favoritesItemContainer}
-            >
-                <Image
-                    source={{ uri: item.place.photo }}
-                    style={styles.itemIcon}
-                />
-                <Text style={{
-                    ...styles.blackPrimaryFontStyle,
-                    flex: 4,
-                    marginHorizontal: 10,
-                    paddingEnd: 20
-                }}>
-                    {item.place.name}
-                </Text>
+            <>
+                <Text>Buscaste: {item.search}</Text>
                 <View
-                    style={{
-                        flex: 2,
-                        justifyContent: 'space-between'
-                    }}
+                    style={styles.listItemContainer}
                 >
-                    <TouchableOpacity
+                    <Image
+                        source={{ uri: place?.photo }}
+                        style={styles.itemIcon}
+                    />
+                    <Text style={{
+                        ...styles.blackPrimaryFontStyle,
+                        flex: 4,
+                        marginHorizontal: 10,
+                        paddingEnd: 20
+                    }}>
+                        {place?.name}
+                    </Text>
+                    <View
                         style={{
-                            alignItems: 'center', flex: 1
+                            flex: 2,
+                            justifyContent: 'space-between'
                         }}
-                        activeOpacity={0.9}
-                        onPress={() => navigator.navigate('ReviewsScreen', { place: item.place._id })}
                     >
-                        <Text style={styles.linkStyle}>
-                            {placeRating.toFixed(2)}
-                        </Text>
-                        <Rating
-                            fractions={2}
-                            imageSize={20}
-                            minValue={1}
-                            ratingBackgroundColor='#FFFFFF'
-                            ratingCount={5}
-                            ratingTextColor='#5856D6'
-                            readonly
-                            showReadOnlyText={false}
-                            startingValue={placeRating}
-                            style={{ flex: 2 }}
-                            tintColor='#EBEBEB'
-                            type='star'
-                        />
-                    </TouchableOpacity>
+                        <TouchableOpacity
+                            style={{
+                                alignItems: 'center', flex: 1
+                            }}
+                            activeOpacity={0.9}
+                            onPress={() => navigator.navigate('ReviewsScreen', { place: place?._id })}
+                        >
+                            <Text style={styles.linkStyle}>
+                                {placeRating.toFixed(2)}
+                            </Text>
+                            <Rating
+                                fractions={2}
+                                imageSize={20}
+                                minValue={1}
+                                ratingBackgroundColor='#FFFFFF'
+                                ratingCount={5}
+                                ratingTextColor='#5856D6'
+                                readonly
+                                showReadOnlyText={false}
+                                startingValue={placeRating}
+                                style={{ flex: 2 }}
+                                tintColor='#EBEBEB'
+                                type='star'
+                            />
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
+            </>
         </TouchableWithoutFeedback>
     );
 };
