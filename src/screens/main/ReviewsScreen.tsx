e9c6c389fef6541ef1b13714134e67ea40a619e2
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-04-20 23:07:18 -0500

    Conditional rendering upon amount of ratings and remove getPlaceRatingAverage query

--- src/screens/main/ReviewsScreen.tsx
+++ src/screens/main/ReviewsScreen.tsx
@@ -22,7 +22,7 @@ const ReviewsScreen = ({ route }: Props) => {
 
     const { place } = route.params;
 
-    const { ratings, ratingAverage, getRatings, getPlaceRatingAverage } = useContext(RatingContext);
+    const { ratings, getRatings } = useContext(RatingContext);
     const { loadPlaceByID } = useContext(PlacesContext);
 
     const [placeInfo, setPlaceInfo] = useState<IPlace>();
@@ -35,7 +35,6 @@ const ReviewsScreen = ({ route }: Props) => {
     useEffect(() => {
         getPlace();
         getRatings(place);
-        getPlaceRatingAverage(place);
     }, [place]);
 
     return (
@@ -43,75 +42,84 @@ const ReviewsScreen = ({ route }: Props) => {
             {(place !== placeInfo?._id)
                 ? <LoadingScreen />
                 : <>
-                    <View style={{ marginTop: top }}>
-                        <Text style={styles.blackTitle}>{placeInfo?.name}</Text>
-                        <Text style={styles.blackTitle}>{ratingAverage.toFixed(2)}</Text>
-                        <Text style={{ ...styles.secondaryFontStyle, alignSelf: 'center' }}>{ratings.total} opiniones</Text>
-                    </View>
-                    <View style={{ flex: 10 }}>
-                        <FlatList
-                            data={ratings.rates}
-                            renderItem={({ item }) => (
-                                <View
-                                    style={{ flex: 1, margin: 20 }}
-                                >
-                                    <View
-                                        style={{ flexDirection: 'row' }}
-                                    >
-                                        <Image
-                                            style={{
-                                                borderRadius: 10,
-                                                flex: 1,
-                                                height: 60,
-                                                width: 60
-                                            }}
-                                            source={(item.user?.photo === '')
-                                                ? require('../../assets/placeholder.png')
-                                                : { uri: item.user?.photo }}
-                                        />
-                                        <View style={{ flex: 5, paddingHorizontal: 10 }}>
-                                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                                <Text style={styles.blackPrimaryFontStyle}>
-                                                    {((item.user?.name!).length > 18) ?
-                                                        (((item.user?.name!).substring(0, 18)) + '...') :
-                                                        item.user?.name}
-                                                </Text>
-                                                <Text style={styles.secondaryFontStyle}>
-                                                    {moment(item.createdAt, "YYYYMMDD").fromNow()}
-                                                </Text>
-                                            </View>
-                                            <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
-                                                <Rating
-                                                    fractions={2}
-                                                    imageSize={20}
-                                                    minValue={1}
-                                                    ratingBackgroundColor='#FFFFFF'
-                                                    ratingCount={5}
-                                                    ratingTextColor='#5856D6'
-                                                    readonly
-                                                    showReadOnlyText={false}
-                                                    startingValue={item.rate}
-                                                    style={{ marginEnd: 5 }}
-                                                    tintColor='#FFFFFF'
-                                                    type='star'
+                    {(ratings.total === 0)
+                        ?
+                        <View style={{ marginTop: top, alignItems: 'center' }}>
+                            <Text style={styles.blackTitle}>{placeInfo?.name}</Text>
+                            <Text style={styles.secondaryFontStyle}>No hay opiniones</Text>
+                        </View>
+                        : <>
+                            <View style={{ marginTop: top, alignItems: 'center' }}>
+                                <Text style={styles.blackTitle}>{placeInfo?.name}</Text>
+                                <Text style={styles.blackTitle}>{placeInfo.rate.$numberDecimal}</Text>
+                                <Text style={styles.secondaryFontStyle}>{ratings.total} opiniones</Text>
+                            </View>
+                            <View style={{ flex: 10 }}>
+                                <FlatList
+                                    data={ratings.rates}
+                                    renderItem={({ item }) => (
+                                        <View
+                                            style={{ flex: 1, margin: 20 }}
+                                        >
+                                            <View
+                                                style={{ flexDirection: 'row' }}
+                                            >
+                                                <Image
+                                                    style={{
+                                                        borderRadius: 10,
+                                                        flex: 1,
+                                                        height: 60,
+                                                        width: 60
+                                                    }}
+                                                    source={(item.user?.photo === '')
+                                                        ? require('../../assets/placeholder.png')
+                                                        : { uri: item.user?.photo }}
                                                 />
-                                                <Text style={styles.secondaryFontStyle}>
-                                                    ({item.rate.toFixed(1)})
-                                                </Text>
+                                                <View style={{ flex: 5, paddingHorizontal: 10 }}>
+                                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                                        <Text style={styles.blackPrimaryFontStyle}>
+                                                            {((item.user?.name!).length > 18) ?
+                                                                (((item.user?.name!).substring(0, 18)) + '...') :
+                                                                item.user?.name}
+                                                        </Text>
+                                                        <Text style={styles.secondaryFontStyle}>
+                                                            {moment(item.createdAt, "YYYYMMDD").fromNow()}
+                                                        </Text>
+                                                    </View>
+                                                    <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+                                                        <Rating
+                                                            fractions={2}
+                                                            imageSize={20}
+                                                            minValue={1}
+                                                            ratingBackgroundColor='#FFFFFF'
+                                                            ratingCount={5}
+                                                            ratingTextColor='#5856D6'
+                                                            readonly
+                                                            showReadOnlyText={false}
+                                                            startingValue={item.rate}
+                                                            style={{ marginEnd: 5 }}
+                                                            tintColor='#FFFFFF'
+                                                            type='star'
+                                                        />
+                                                        <Text style={styles.secondaryFontStyle}>
+                                                            ({item.rate.toFixed(1)})
+                                                        </Text>
+                                                    </View>
+                                                </View>
                                             </View>
+                                            {(item.comments !== '') &&
+                                                <View>
+                                                    <Text style={styles.secondaryFontStyle}>
+                                                        {item.comments}
+                                                    </Text>
+                                                </View>
+                                            }
                                         </View>
-                                    </View>
-                                    {(item.comments !== '') &&
-                                        <View>
-                                            <Text style={styles.secondaryFontStyle}>
-                                                {item.comments}
-                                            </Text>
-                                        </View>
-                                    }
-                                </View>
-                            )}
-                        />
-                    </View>
+                                    )}
+                                />
+                            </View>
+                        </>
+                    }
                 </>
             }
         </>
