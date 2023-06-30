ccacc96c0c4212955a72a58f54db21b45d804caf
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-06-29 23:06:20 -0500

    Update screen design and add method to log out

--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,25 +1,37 @@
 import React, { useContext } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { PermissionsContext } from '../context';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { AuthContext, PermissionsContext } from '../context';
 import { styles } from '../theme/AppTheme';
 
 const PermissionsScreen = () => {
 
     const { askLocationPermission } = useContext(PermissionsContext);
+    const { logOut } = useContext(AuthContext);
 
     return (
         <View
-            style={styles.center}
+            style={{ ...styles.center, paddingHorizontal: 60 }}
         >
-            <Text style={styles.blackPrimaryFontStyle}>Es necesario el uso del GPS para usar esta aplicación</Text>
-            <View style={{ ...styles.buttonContainer, backgroundColor: '#FFFFFF' }}>
+            <Image source={require('../assets/location.png')} style={{ height: 73, marginBottom: 23, width: 73 }} />
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 18, fontWeight: '600', letterSpacing: -0.36, textAlign: 'center' }}>Es necesario el acceso al GPS para utilizar esta aplicación</Text>
+            </View>
+            <View style={{ marginTop: 23, width: '100%' }}>
                 <TouchableOpacity
                     activeOpacity={0.8}
-                    style={styles.buttonSearch}
+                    style={{ backgroundColor: '#207CFD', borderRadius: 8, paddingHorizontal: 14, paddingVertical: 10, alignItems: 'center', justifyContent: 'center' }}
                     onPress={askLocationPermission}
                 >
-                    <Text style={styles.buttonSearchText}>Permisos</Text>
+                    <Text style={{ color: 'rgba(250, 250, 250, 0.98)', fontSize: 16, fontWeight: '500', lineHeight: 22, letterSpacing: -0.32 }}>Administrar permisos</Text>
                 </TouchableOpacity>
+            </View>
+            <View style={{ marginTop: 15, justifyContent: 'center' }}>
+            <TouchableOpacity
+                activeOpacity={1.0}
+                onPress={logOut}
+            >
+            <Text style={{ color: '#207CFD', fontSize: 13, fontWeight: '500', lineHeight: 15, letterSpacing: -0.26 }}>Cerrar sesión</Text>
+            </TouchableOpacity>
             </View>
         </View>
     );
