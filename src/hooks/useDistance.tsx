473dc8aa488e7658dbc45147112b573e2c868192
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-01 13:53:31 -0500

    Hook to calculate distance between coords

---
+++ src/hooks/useDistance.tsx
@@ -0,0 +1,15 @@
+const useDistance = (lat1: number, lon1: number, lat2: number, lon2: number, unit: string) => {
+    var radlat1 = Math.PI * lat1 / 180;
+    var radlat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2;
+    var radtheta = Math.PI * theta / 180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") { dist = dist * 1.609344; }
+    if (unit == "M") { dist = dist * 0.8684; }
+    return dist;
+};
+
+export default useDistance;
