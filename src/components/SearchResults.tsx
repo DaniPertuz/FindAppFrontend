6110bdd874e6d9d5835ba7042952d8247f8e87de
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-02 17:45:08 -0500

    Remove unused hook

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
