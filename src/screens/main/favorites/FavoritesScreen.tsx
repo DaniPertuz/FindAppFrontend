4373e633d0520b03e50d2de9ec1543d2316f3345
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-02 16:27:47 -0500

    Implementation of TopButtons component

--- src/screens/main/favorites/FavoritesScreen.tsx
+++ src/screens/main/favorites/FavoritesScreen.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { AuthContext, PlacesContext } from '../../../context';
+import TopButtons from '../../../components/TopButtons';
 import FavoriteItem from './FavoriteItem';
 import LoadingScreen from '../../LoadingScreen';
 import { IFavorites } from '../../../interfaces';
@@ -47,51 +48,7 @@ const FavoritesScreen = () => {
                         paddingTop: 70
                     }}
                 >
-                    <View style={{ flexDirection: 'row' }}>
-                        <View
-                            style={{
-                                alignItems: 'center',
-                                backgroundColor: 'rgba(250, 250, 250, 0.98)',
-                                borderRadius: 8,
-                                flex: 5,
-                                flexDirection: 'row',
-                                paddingHorizontal: 20,
-                                marginEnd: 20,
-                                shadowColor: "#000",
-                                shadowOffset: {
-                                    width: 0,
-                                    height: 1,
-                                },
-                                shadowOpacity: 0.22,
-                                shadowRadius: 2.22,
-                                elevation: 3
-                            }}
-                        >
-                            <TouchableOpacity
-                                activeOpacity={1.0}
-                                onPress={() => { }}
-                            >
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Image
-                                        source={require('../../../assets/search.png')}
-                                        style={{ height: 16, width: 16 }}
-                                    />
-                                    <View style={{ marginHorizontal: 10 }}>
-                                        <Text style={{ color: '#858585', fontSize: 14, fontWeight: '500', lineHeight: 20, letterSpacing: -0.28 }}>
-                                            Escribe una palabra o frase
-                                        </Text>
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <TouchableOpacity
-                            activeOpacity={1.0}
-                            onPress={() => navigation.navigate('EditProfileScreen')}
-                            style={user?.photo !== '' ? { backgroundColor: 'rgba(250, 250, 250, 0.98)', borderRadius: 50, flex: 1, justifyContent: 'center', alignItems: 'center' } : { backgroundColor: 'rgba(250, 250, 250, 0.98)', borderColor: '#081023', borderWidth: 1, borderRadius: 50, flex: 1, justifyContent: 'center', alignItems: 'center' }}
-                        >
-                            <Image source={(user?.photo === '' ? require('../../../assets/FA_Color.png') : { uri: user?.photo })} style={user?.photo !== '' ? { height: 50, width: 50, borderRadius: 50, borderColor: '#081023', borderWidth: 1 } : { height: 50, width: 50, borderRadius: 50 }} />
-                        </TouchableOpacity>
-                    </View>
+                    <TopButtons />
                     <View style={{ marginTop: 20 }}>
                         <Text style={{ color: '#081023', fontSize: 14, fontWeight: '700', lineHeight: 18 }}>Favoritos</Text>
                     </View>
