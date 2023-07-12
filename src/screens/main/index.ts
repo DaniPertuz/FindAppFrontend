027dc9c7b78b01484b293618f0fdc755ae98212c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-07-12 17:59:13 -0500

    Include new screens to index files

--- src/screens/main/index.ts
+++ src/screens/main/index.ts
@@ -3,7 +3,7 @@ export { FavoritesScreen, FavoriteItem } from './favorites';
 export { HistoryScreen } from './history';
 export { default as MainScreen } from './MainScreen';
 export { default as MapScreen } from './MapScreen';
-export { RateScreen, RateItem } from './rating';
+export { RateScreen, RateItem, RatingsScreen } from './rating';
 export { default as PlaceDetailsScreen } from './PlaceDetailsScreen';
 export { default as ProductDetailsScreen } from './ProductDetailsScreen';
 export { default as SearchScreen } from './SearchScreen';
--- src/screens/main/rating/index.ts
+++ src/screens/main/rating/index.ts
@@ -1,2 +1,4 @@
 export { default as RateItem } from './RateItem';
 export { default as RateScreen } from './RateScreen';
+export { default as RatingsScreen } from './RatingsScreen';
+export { default as RatingListItem } from './RatingListItem';
