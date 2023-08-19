7b4a6e7913d9ed3f1254e4b4842c30c49bf5f575
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-19 10:41:50 -0500

    Remove navigator screen options

--- src/navigation/FavoritesNavigator.tsx
+++ src/navigation/FavoritesNavigator.tsx
@@ -13,32 +13,10 @@ export const FavoritesNavigator = () => {
     const navigator = useNavigation();
 
     return (
-        <Stack.Navigator
-            screenOptions={{
-                headerStyle: {
-                    elevation: 0,
-                    shadowColor: 'transparent'
-                },
-                cardStyle: {
-                    backgroundColor: 'white'
-                }
-            }}
-        >
+        <Stack.Navigator>
             <Stack.Screen
                 name="FavoritesScreen"
-                options={{
-                    headerShown: false,
-                    headerTintColor: '#FFFFFF',
-                    headerTitleAlign: 'center',
-                    headerLeft: () => (
-                        <TouchableOpacity
-                            activeOpacity={0.9}
-                            style={{ marginLeft: 15 }}
-                            onPress={() => navigator.reset({ index: 0, routes: [{ name: 'MainScreen' }] })}
-                        >
-                        </TouchableOpacity>
-                    )
-                }}
+                options={{ headerShown: false }}
                 component={FavoritesScreen}
             />
             <Stack.Screen name="MapScreen" options={{ title: '', headerShown: false }} component={MapScreen} />
