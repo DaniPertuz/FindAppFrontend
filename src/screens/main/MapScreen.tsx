3036dc605f237c8828e9daeaf7b1092f916c8953
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-20 23:11:31 -0500

    Remove unused code

--- src/screens/main/MapScreen.tsx
+++ src/screens/main/MapScreen.tsx
@@ -38,7 +38,6 @@ const MapScreen = ({ route, navigation }: Props) => {
     const [direction, setDirection] = useState<Step>({ distance: '', end_location: currentUserLocation, html_instructions: '', maneuver: '' });
     const [steps, setSteps] = useState<Step[]>([]);
     const [deviceFormat, setDeviceFormat] = useState(false);
-    const [movement, setMovement] = useState<boolean>(false);
     const [follow, setFollow] = useState(false);
     const [modalVisible, setModalVisible] = useState(true);
     const [modalFollowVisible, setModalFollowVisible] = useState(false);
@@ -184,21 +183,6 @@ const MapScreen = ({ route, navigation }: Props) => {
             default:
                 return useIcons('Car', 46, 46);
         }
-    };
-
-    const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-        const R = 6371;
-        const dLat = (lat2 - lat1) * (Math.PI / 180);
-        const dLon = (lon2 - lon1) * (Math.PI / 180);
-        const a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1 * (Math.PI / 180)) *
-            Math.cos(lat2 * (Math.PI / 180)) *
-            Math.sin(dLon / 2) *
-            Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const distance = R * c;
-        return distance;
     };
 
     const handleBackButtonClick = () => {
