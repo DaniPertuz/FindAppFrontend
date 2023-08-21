6b62c791af1e9e4134b660222c68c4fc239101b7
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-08-20 20:32:53 -0500

    MapIcon component

---
+++ src/components/MapIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { styles } from '../theme/AppTheme';
+
+const MapIcon = () => <Image source={require('../assets/FA_Color.png')} style={styles.mapIcon} />;
+
+export default MapIcon;
