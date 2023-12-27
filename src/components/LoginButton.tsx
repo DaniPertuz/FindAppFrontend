87eef5fd2cd46555e63a3faa067410e273ec453f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-27 17:36:21 -0500

    Update email and password validation

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,15 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { ActivityIndicator, Keyboard, Text, TouchableOpacity } from 'react-native';
+import { ActivityIndicator, Text, TouchableOpacity } from 'react-native';
 import { AuthContext } from '../context';
 import { styles } from '../theme/AppTheme';
 
 interface Props {
     email: string;
     password: string;
-    handleFieldLength: (email: boolean, password: boolean) => void;
+    handleFieldLength: (emailEmpty: boolean, passwordEmpty: boolean) => void
 }
 
-const LoginButton = ({ email = '', password = '', handleFieldLength }: Props) => {
+const LoginButton = ({ email, password, handleFieldLength }: Props) => {
 
     const { signIn, errorMessage } = useContext(AuthContext);
     const [loading, setLoading] = useState(false);
@@ -19,22 +19,7 @@ const LoginButton = ({ email = '', password = '', handleFieldLength }: Props) =>
     }, [errorMessage]);
 
     const onLogin = async () => {
-        Keyboard.dismiss();
-
-        if (email.length === 0 && password.length !== 0) {
-            handleFieldLength(true, false);
-            return;
-        }
-
-        if (email.length !== 0 && password.length === 0) {
-            handleFieldLength(false, true);
-            return;
-        }
-
-        if (email.length === 0 && password.length === 0) {
-            handleFieldLength(true, true);
-            return;
-        }
+        handleFieldLength(email.length === 0, password.length === 0);
 
         if (email.length !== 0 && password.length !== 0) {
             setLoading(true);
