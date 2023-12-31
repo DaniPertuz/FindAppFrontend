48f7586f3bcdcc37830632f69e7b7e4ecb5be4cc
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2023-12-31 14:09:08 -0500

    Refactor code and update conditional rendering

--- src/screens/main/profile/EditProfileScreen.tsx
+++ src/screens/main/profile/EditProfileScreen.tsx
@@ -1,29 +1,36 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Image, KeyboardAvoidingView, Modal, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ImagePickerResponse, launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import { useIsFocused } from '@react-navigation/native';
 import { StackScreenProps } from '@react-navigation/stack';
 import Toast from 'react-native-root-toast';
 
 import { AuthContext, PlacesContext, UsersContext } from '../../../context';
 import StatusBarComponent from '../../../components/StatusBarComponent';
-import { useIcons } from '../../../hooks/useIcons';
-import { IRatingList } from '../../../interfaces';
+import { useIcons } from '../../../hooks';
+import { IRatingList, IUser, roles } from '../../../interfaces';
 import { RootStackParams } from '../../../navigation';
+import LoadingScreen from '../../LoadingScreen';
 
 import { styles } from '../../../theme/AppTheme';
 
 interface Props extends StackScreenProps<RootStackParams, 'EditProfileScreen'> { }
 
 const EditProfileScreen = ({ navigation }: Props) => {
-    const { top } = useSafeAreaInsets();
     const isFocused = useIsFocused();
 
-    const [response, setResponse] = useState<any>(null);
-    const [userDB, setUserDB] = useState<any>(null);
+    const [response, setResponse] = useState<ImagePickerResponse>();
+    const [userDB, setUserDB] = useState<IUser>({
+        role: roles.CLIENT,
+        name: '',
+        email: '',
+        password: '',
+        status: false,
+        photo: ''
+    });
     const [modalVisible, setModalVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState('');
     const [totalHistory, setTotalHistory] = useState<number>(0);
     const [totalFavorites, setTotalFavorites] = useState<number>(0);
     const [userRatings, setUserRatings] = useState<IRatingList>({ total: 0, rates: [] });
@@ -35,6 +42,45 @@ const EditProfileScreen = ({ navigation }: Props) => {
     const load = async () => {
         const usr = await loadUserByID(user?._id!);
         setUserDB(usr);
+    };
+
+    const addPhoto = () => {
+        launchCamera({
+            mediaType: 'photo',
+            quality: 0.8
+        }, (response) => {
+            if (!response || response.didCancel || !response.assets) return;
+
+            let imageUri = response.assets[0]?.uri || '';
+            setSelectedImage(imageUri);
+            setResponse(response);
+        });
+    };
+
+    const addGalleryImage = () => {
+        launchImageLibrary({
+            mediaType: 'photo',
+            quality: 0.8
+        }, (response) => {
+            if (!response || response.didCancel || !response.assets) return;
+
+            let imageUri = response.assets[0]?.uri || '';
+            setSelectedImage(imageUri);
+            setResponse(response);
+        });
+    };
+
+    const onUpdate = async () => {
+        if (response && selectedImage !== '') {
+            const photoURL = await updatePhoto(response, userDB._id!);
+            updateUser(userDB._id!, userDB.name!, photoURL);
+            Toast.show('Foto actualizada', { duration: Toast.durations.SHORT, position: Toast.positions.BOTTOM });
+        }
+    };
+
+    const handleBackButtonClick = () => {
+        setModalVisible(false);
+        return true;
     };
 
     useEffect(() => {
@@ -79,40 +125,6 @@ const EditProfileScreen = ({ navigation }: Props) => {
         };
     }, []);
 
-    const addPhoto = () => {
-        launchCamera({
-            mediaType: 'photo',
-            quality: 0.8
-        }, setResponse);
-    };
-
-    const addGalleryImage = () => {
-        launchImageLibrary({
-            mediaType: 'photo',
-            quality: 0.8
-        }, setResponse);
-    };
-
-    const onUpdate = async () => {
-        if (response.didCancel) return;
-        if (!response.assets) return;
-
-        if (response && response.assets[0].uri !== '') {
-            const photoURL = await updatePhoto(response, user?._id!);
-            updateUser(user?._id!, user?.name!, photoURL);
-            Toast.show('Foto actualizada', { duration: Toast.durations.SHORT, position: Toast.positions.BOTTOM });
-        }
-    };
-
-    const updateMainPhoto = () => {
-        setModalVisible(true);
-    };
-
-    const handleBackButtonClick = () => {
-        setModalVisible(false);
-        return true;
-    };
-
     useEffect(() => {
         const navFocusListener = navigation.addListener('blur', () => {
             handleBackButtonClick();
@@ -123,124 +135,123 @@ const EditProfileScreen = ({ navigation }: Props) => {
 
     return (
         <View style={styles.mainBackground}>
-            <KeyboardAvoidingView
-                style={{ ...styles.flexOne }}
-                behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}
-            >
-                <StatusBarComponent color='rgba(104, 110, 222, 0)' theme='dark-content' />
-                <ScrollView
-                    style={styles.editProfileScrollView}
-                    contentContainerStyle={styles.justifyContentCenter}>
-                    <View style={styles.flexDirectionRowJustifyCenter}>
-                        <Image
-                            source={(!userDB || userDB.photo === '')
-                                ? require('../../../assets/FA_Color.png')
-                                : (response?.assets && response.assets[0].uri !== '')
-                                    ? { uri: response.assets[0].uri }
-                                    : { uri: userDB.photo }}
-                            style={styles.profileAvatar}
-                        />
-                        <TouchableOpacity
-                            activeOpacity={1}
-                            onPress={updateMainPhoto}
-                            style={styles.editProfilePhotoButton}
-                        >
-                            {useIcons('Camera', 30, 30)}
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ ...styles.flexDirectionRowJustifyCenter, ...styles.mediumMarginTop }}>
-                        <Text style={styles.editProfileUserNameText}>
-                            {userDB?.name}
-                        </Text>
-                        <TouchableOpacity
-                            activeOpacity={0.9}
-                            onPress={() => { navigation.navigate('UpdateProfileScreen', { user: userDB }); }}
-                            style={styles.editProfileButton}
-                        >
-                            {useIcons('Edit', 20, 20)}
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ ...styles.flexDirectionRowJustifyCenter, ...styles.tinyMarginTop }}>
-                        <Text style={styles.placeholderText}>{userDB?.email}</Text>
-                    </View>
-                    <View style={{ ...styles.flexDirectionRowJustifyAround, ...styles.mediumMarginTop }}>
-                        <View style={styles.largeItem}>
+            {(!totalHistory || !totalFavorites || !userRatings)
+                ? <LoadingScreen />
+                :
+                <KeyboardAvoidingView style={{ ...styles.flexOne }} behavior={(Platform.OS === 'ios') ? 'padding' : 'height'}>
+                    <StatusBarComponent color='rgba(104, 110, 222, 0)' theme='dark-content' />
+                    <ScrollView style={styles.editProfileScrollView} contentContainerStyle={styles.justifyContentCenter}>
+                        <View style={styles.flexDirectionRowJustifyCenter}>
+                            <Image
+                                source={(!userDB || userDB.photo === '')
+                                    ? require('../../../assets/FA_Color.png')
+                                    : (response?.assets && response.assets[0].uri !== '')
+                                        ? { uri: response.assets[0].uri }
+                                        : { uri: userDB.photo }}
+                                style={styles.profileAvatar}
+                            />
                             <TouchableOpacity
-                                activeOpacity={1.0}
-                                style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
-                                onPress={() => navigation.navigate('HistoryScreen')}
+                                activeOpacity={1}
+                                onPress={() => setModalVisible(true)}
+                                style={styles.editProfilePhotoButton}
                             >
-                                {useIcons('History', 33, 33)}
-                                <View style={styles.smallMediumMarginTop}>
-                                    <Text style={styles.plainSmallText}>Historial</Text>
-                                </View>
-                                <View style={styles.tinyMarginTop}>
-                                    <Text style={styles.largeItemText}>{totalHistory} viajes</Text>
-                                </View>
+                                {useIcons('Camera', 30, 30)}
                             </TouchableOpacity>
                         </View>
-                        <View style={styles.largeItem}>
+                        <View style={{ ...styles.flexDirectionRowJustifyCenter, ...styles.mediumMarginTop }}>
+                            <Text style={styles.editProfileUserNameText}>
+                                {userDB?.name}
+                            </Text>
                             <TouchableOpacity
-                                activeOpacity={1.0}
-                                style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
-                                onPress={() => navigation.navigate('FavoritesNavigator')}
+                                activeOpacity={0.9}
+                                onPress={() => { navigation.navigate('UpdateProfileScreen', { user: userDB }); }}
+                                style={styles.editProfileButton}
                             >
-                                {useIcons('HeartFavorite', 33, 33)}
-                                <View style={styles.smallMediumMarginTop}>
-                                    <Text style={styles.plainSmallText}>Favoritos</Text>
-                                </View>
-                                <View style={styles.tinyMarginTop}>
-                                    <Text style={styles.largeItemText}>{totalFavorites} lugares</Text>
-                                </View>
+                                {useIcons('Edit', 20, 20)}
                             </TouchableOpacity>
                         </View>
-                        <View style={styles.largeItem}>
-                            <TouchableOpacity
-                                activeOpacity={1.0}
-                                style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
-                                onPress={() => navigation.navigate('RatingsScreen')}
-                            >
-                                {useIcons('Star', 33, 33)}
-                                <View style={styles.smallMediumMarginTop}>
-                                    <Text style={styles.plainSmallText}>Calificaciones</Text>
-                                </View>
-                                <View style={styles.tinyMarginTop}>
-                                    <Text style={styles.largeItemText}>{userRatings.total} lugares</Text>
-                                </View>
-                            </TouchableOpacity>
+                        <View style={{ ...styles.flexDirectionRowJustifyCenter, ...styles.tinyMarginTop }}>
+                            <Text style={styles.placeholderText}>{userDB?.email}</Text>
                         </View>
-                    </View>
-                    <View style={styles.mediumMarginTop}>
-                        <Text style={styles.grayLabel}>Nombre de usuario</Text>
-                        <View style={{ ...styles.flexDirectionRow, marginTop: 4 }}>
-                            <View style={styles.editProfileIconMargins}>
-                                {useIcons('User', 18, 18)}
+                        <View style={{ ...styles.flexDirectionRowJustifyAround, ...styles.mediumMarginTop }}>
+                            <View style={styles.largeItem}>
+                                <TouchableOpacity
+                                    activeOpacity={1.0}
+                                    style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
+                                    onPress={() => navigation.navigate('HistoryScreen')}
+                                >
+                                    {useIcons('History', 33, 33)}
+                                    <View style={styles.smallMediumMarginTop}>
+                                        <Text style={styles.plainSmallText}>Historial</Text>
+                                    </View>
+                                    <View style={styles.tinyMarginTop}>
+                                        <Text style={styles.largeItemText}>{totalHistory} viajes</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
-                            <Text style={styles.editProfileMediumText}>{userDB?.name}</Text>
-                        </View>
-                    </View>
-                    <View style={styles.mediumLargeMarginTop}>
-                        <Text style={styles.grayLabel}>Email</Text>
-                        <View style={{ ...styles.flexDirectionRow, marginTop: 4 }}>
-                            <View style={styles.editProfileIconMargins}>
-                                {useIcons('Envelope', 18, 18)}
+                            <View style={styles.largeItem}>
+                                <TouchableOpacity
+                                    activeOpacity={1.0}
+                                    style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
+                                    onPress={() => navigation.navigate('FavoritesNavigator')}
+                                >
+                                    {useIcons('HeartFavorite', 33, 33)}
+                                    <View style={styles.smallMediumMarginTop}>
+                                        <Text style={styles.plainSmallText}>Favoritos</Text>
+                                    </View>
+                                    <View style={styles.tinyMarginTop}>
+                                        <Text style={styles.largeItemText}>{totalFavorites} lugares</Text>
+                                    </View>
+                                </TouchableOpacity>
                             </View>
-                            <Text style={styles.editProfileMediumText}>{userDB?.email}</Text>
+                            <View style={styles.largeItem}>
+                                <TouchableOpacity
+                                    activeOpacity={1.0}
+                                    style={{ ...styles.alignItemsCenter, ...styles.extraSmallMarginTop }}
+                                    onPress={() => navigation.navigate('RatingsScreen')}
+                                >
+                                    {useIcons('Star', 33, 33)}
+                                    <View style={styles.smallMediumMarginTop}>
+                                        <Text style={styles.plainSmallText}>Calificaciones</Text>
+                                    </View>
+                                    <View style={styles.tinyMarginTop}>
+                                        <Text style={styles.largeItemText}>{userRatings.total} lugares</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                    </View>
-                    <View style={styles.mediumLargeMarginTop}>
-                        <View style={styles.alignItemsBaseline}>
-                            <TouchableOpacity
-                                activeOpacity={1.0}
-                                style={styles.alignItemsBaseline}
-                                onPress={logOut}
-                            >
-                                <Text style={styles.logOutText}>Cerrar sesión</Text>
-                            </TouchableOpacity>
+                        <View style={styles.mediumMarginTop}>
+                            <Text style={styles.grayLabel}>Nombre de usuario</Text>
+                            <View style={{ ...styles.flexDirectionRow, marginTop: 4 }}>
+                                <View style={styles.editProfileIconMargins}>
+                                    {useIcons('User', 18, 18)}
+                                </View>
+                                <Text style={styles.editProfileMediumText}>{userDB?.name}</Text>
+                            </View>
                         </View>
-                    </View>
-                </ScrollView>
-            </KeyboardAvoidingView>
+                        <View style={styles.mediumLargeMarginTop}>
+                            <Text style={styles.grayLabel}>Email</Text>
+                            <View style={{ ...styles.flexDirectionRow, marginTop: 4 }}>
+                                <View style={styles.editProfileIconMargins}>
+                                    {useIcons('Envelope', 18, 18)}
+                                </View>
+                                <Text style={styles.editProfileMediumText}>{userDB?.email}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.mediumLargeMarginTop}>
+                            <View style={styles.alignItemsBaseline}>
+                                <TouchableOpacity
+                                    activeOpacity={1.0}
+                                    style={styles.alignItemsBaseline}
+                                    onPress={logOut}
+                                >
+                                    <Text style={styles.logOutText}>Cerrar sesión</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            }
             <Modal
                 animationType="slide"
                 transparent={true}
