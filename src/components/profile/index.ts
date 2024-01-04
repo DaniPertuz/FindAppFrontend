fc0903d7fa9c02fd622a9b9a4952bd9c935144fd
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-04 00:07:06 -0500

    Add component to index file

---
+++ src/components/profile/index.ts
@@ -0,0 +1,3 @@
+export { default as ProfileBody } from './ProfileBody';
+export { default as ProfileHeader } from './ProfileHeader';
+export { default as ProfileSubheader } from './ProfileSubheader';
