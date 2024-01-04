38a543b652d863b3d7aeed35b3989a126ccc7a18
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-01-03 22:14:03 -0500

    Implement FavoritesList component

--- src/screens/main/favorites/FavoritesScreen.tsx
+++ src/screens/main/favorites/FavoritesScreen.tsx
@@ -1,19 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 
 import { AuthContext, PlacesContext } from '../../../context';
+import { FavoritesList } from '../../../components/favorites';
 import StatusBarComponent from '../../../components/StatusBarComponent';
 import TopButtons from '../../../components/TopButtons';
-import FavoriteItem from './FavoriteItem';
 import LoadingScreen from '../../LoadingScreen';
 import { IFavorites } from '../../../interfaces';
 
 import { styles } from '../../../theme/AppTheme';
 
 const FavoritesScreen = () => {
-
-    const navigation = useNavigation();
 
     const init = { total: 0, favorites: [] };
 
@@ -38,35 +35,7 @@ const FavoritesScreen = () => {
 
     return (
         <>
-            {(!display)
-                ? <LoadingScreen />
-                :
-                <View style={styles.favoriteScreenContainer}>
-                    <StatusBarComponent color='rgba(104, 110, 222, 0)' theme='dark-content' />
-                    <TopButtons />
-                    <View style={styles.mediumMarginTop}>
-                        <Text style={styles.boldMediumText}>Favoritos</Text>
-                    </View>
-                    <View style={styles.smallMarginTop}>
-                        {(favorites.total === 0)
-                            ?
-                            <View style={styles.alignItemsJustifyContentCenter}>
-                                <Text style={styles.boldMediumText}>No hay favoritos aún</Text>
-                            </View>
-                            :
-                            <FlatList
-                                data={favorites.favorites}
-                                keyExtractor={(item) => item.place._id}
-                                renderItem={({ item }) => {
-                                    return (
-                                        <FavoriteItem item={item} />
-                                    );
-                                }}
-                            />
-                        }
-                    </View>
-                </View>
-            }
+            {(!display) ? <LoadingScreen /> : <FavoritesList favorites={favorites} />}
         </>
     );
 };
